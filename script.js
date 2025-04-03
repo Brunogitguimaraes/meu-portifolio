01b442943d741617a7abee5d713848c4bbf9ba9b
Brunogitguimaraes <brunodasilvaguimaraes609@gmail.com>
2025-04-03 19:09:29 -0300

    adicionado efeitos ScrolReveal

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-function linkWatssapp(event) {
 
+const sr = ScrollReveal({ reset: true });
 
-    const nome = document.querySelector('.campo-form').value.trim();
-    const mensagem = document.getElementById('textarea-mensagem').value.trim();
-
-    if (!nome || !mensagem) {
-        alert('Por favor, preencha seu nome e sua mensagem antes de enviar.');
-        return;
-    }
-
-    const numero = '5561992938167';
-    const texto = `Olá, meu nome é ${nome}, ${mensagem}`;
-    const msgFormatada = encodeURIComponent(texto);
-    const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
-
-    window.open(url, '_blank');
-}
-
-
-
-
+// Inicializando o ScrollReveal
+ScrollReveal().reveal('#home .sobre', {
+    duration: 1000,
+    delay: 200,
+    origin: 'bottom',
+    distance: '50px',
+    opacity: 0,
+    easing: 'ease-out',
+  });
+  
+  ScrollReveal().reveal('#inicio .cabeçalho', {
+    duration: 1000,
+    delay: 400,
+    origin: 'left',
+    distance: '50px',
+    opacity: 0,
+  });
+  
+  ScrollReveal().reveal('#projetos .titulo-secao', {
+    duration: 1000,
+    delay: 200,
+    origin: 'top',
+    distance: '50px',
+    opacity: 0,
+  });
+  
+  ScrollReveal().reveal('.card-projeto', {
+    duration: 1000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '50px',
+    opacity: 0,
+    interval: 200, // Intervalo entre animações de cartões
+  });
+  
+  ScrollReveal().reveal('#sobre', {
+    duration: 1000,
+    delay: 300,
+    origin: 'right',
+    distance: '50px',
+    opacity: 0,
+  });
+  
+  ScrollReveal().reveal('#contatos .titulo-secao', {
+    duration: 1000,
+    delay: 300,
+    origin: 'top',
+    distance: '50px',
+    opacity: 0,
+  });
+  
+  ScrollReveal().reveal('.botoes-contatos a', {
+    duration: 1000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '50px',
+    opacity: 0,
+    interval: 200, // Intervalo entre animações de botões de contato
+  });
   
